18e57d3848fdefb3f16468109c7abedbe4178d5b
Nilesh Pant <nileshpant112@gmail.com>
2020-12-18 21:44:07 +0530

    some change in model structure

--- api/models.py
+++ api/models.py
@@ -4,12 +4,12 @@ from django.db import models
 class BankDetails(models.Model):
     ifsc = models.CharField(max_length=60)
     bank_id = models.IntegerField()
-    branch = models.CharField(max_length=60)
-    address = models.CharField(max_length=100)
-    city = models.CharField(max_length=20)
+    branch = models.CharField(max_length=200)
+    address = models.CharField(max_length=500)
+    city = models.CharField(max_length=100)
     district = models.CharField(max_length=60)
     state = models.CharField(max_length=60)
-
+    bank_name = models.CharField(max_length=100, default="Central Bank Of India")
 
     def __str__(self):
         return self.ifsc
